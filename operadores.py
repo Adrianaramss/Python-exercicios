bd608bcd94c1eb9b6cbb15539e9f15b0ce479c85
Adriana <adrianascosta9@gmail.com>
2023-04-10 15:52:13 -0300

    exerc

---
+++ operadores.py
@@ -0,0 +1,8 @@
+numero1 = 10
+numero2 = 20 
+
+print (numero1 + numero2)
+print (numero1 - numero2)
+print (numero1 * numero2)
+print (numero1 / numero2)
+print (numero1 // numero2)
---
+++ variavel.py
@@ -0,0 +1,12 @@
+print("hello word")
+
+#variaveis
+idade = 27 # criando uma variavel
+nome = "adriana ramos"
+estudando = True
+print (type(idade))
+print (type(nome))
+print (type(estudando))
+
+linguagem = input('qual a linguagem que vc esta estudando?')
+print ("a linguagem que estou estudando é ",linguagem)
